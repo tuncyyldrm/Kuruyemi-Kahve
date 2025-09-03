60095dc7b92a48b6f4ec82680a515c4c59459e0a
Tuncay Yıldırım <tuncyyldrm@gmail.com>
2025-09-03 14:26:21 +0300

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+return (
+<footer className="border-t mt-8 bg-white">
+<div className="max-w-6xl mx-auto px-4 py-6 text-sm flex items-center justify-between">
+<div>© {new Date().getFullYear()} Gurme Kuruyemiş & Kahve</div>
+<div className="text-stone-500">KVKK | İade & Değişim | Teslimat Koşulları</div>
+</div>
+</footer>
+)
+}
